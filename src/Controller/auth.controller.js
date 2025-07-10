027845cadd98c2bc715c2c147b408db787de04cd
bensonwhalesdev <lawrencebenson18@hotmail.com>
2025-07-10 15:08:18 -0700

    files updated

--- src/Controller/auth.controller.js
+++ src/Controller/auth.controller.js
@@ -28,12 +28,11 @@ const register = async (req, res) => {
 
     const token = createToken(user._id);
 
-    res
-      .cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .status(201)
       .json({
@@ -65,13 +64,12 @@ const login = async (req, res) => {
 
     const token = createToken(user._id);
 
-    res
-      .cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      })
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+     })
       .status(200)
       .json({
         message: "Logged in successfully",
